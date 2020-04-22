daa3af2749d33aa6e3a086f9bd3462f87383c13e
jiangben <2284634727@qq.com>
2020-04-22 17:09:17 +0800

    update

--- flutter_01/lib/main.dart
+++ flutter_01/lib/main.dart
@@ -33,7 +33,7 @@ class App extends StatelessWidget {
         const Locale('zh','CH'),
         const Locale('en','US'),
       ],
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedodeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
